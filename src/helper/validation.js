c6de87f6cf6882538e670aa923744313b37fd842
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-10-31 20:39:10 +0300

    added express-middleware

--- src/helper/validation.js
+++ src/helper/validation.js
@@ -10,18 +10,18 @@ function isValidUserId(req, res, next) {
 function isValidUserData(req, res, next) {
     const { name, surname, email, pwd } = req.body;
 
-    if (!name) throw new Error('значение имени пустое');
-    if (!isNaN(name)) throw new Error('значение имени не может быть числом');
+    if (!name) throw new Error('значение name пустое');
+    if (!isNaN(name)) throw new Error('значение name не может быть числом');
 
-    if (!surname) throw new Error('значение фамилии пустое');
-    if (!isNaN(surname)) throw new Error('значение фамилии не может быть числом');
+    if (!surname) throw new Error('значение surname пустое');
+    if (!isNaN(surname)) throw new Error('значение surname не может быть числом');
 
 
-    if (!email) throw new Error('значение почты пустое');
-    if (!/^[a-zA-Z0-9\.\s]+@[a-z]+.[a-z]{2,4}$/gm.test(email)) throw new Error('значение почты неверное');
+    if (!email) throw new Error('значение email пустое');
+    if (!/^[a-zA-Z0-9\.\s]+@[a-z]+.[a-z]{2,4}$/gm.test(email)) throw new Error('значение email неверное');
 
-    if (!pwd) throw new Error('значение пароля пустое');
-    if (!/^[A-Z]+[a-z]+\W+[0-9]+$/gm.test(pwd)) throw new Error('значение пароля неверное');
+    if (!pwd) throw new Error('значение pwd пустое');
+    if (!/^[A-Z]+[a-z]+\W+[0-9]+$/gm.test(pwd)) throw new Error('значение pwd неверное');
 
     next();
 }
